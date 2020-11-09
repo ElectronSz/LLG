629c4c95d3b55a42d4672c871b40add6b8e1e767
ElectronSz <teamallghostanon@gmail.com>
2020-11-09 20:42:20 +0200

    Trying to create a custom logger

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-function llg() {
-    
-    if(arguments.length == 0) {
-        
-        console.log("Grap a 🍺", " nothing to output!");
-        
-    }
-    else if(arguments.length == 1){
-         for (let index = 0; index < arguments.length; index++) {
-            const element = arguments[index];
-            console.log("🤑  "+JSON.stringify(element));
-            
-        }
-    }
-    else {
-        let elements = []
 
-        for (let index = 0; index < arguments.length; index++) {
-            const element = arguments[index];
+class Llg {
+    constructor(token) {
+        this.token = token;
+    }
+
+    log(message, level, date) {
+        if(this.token == "" || this.token == undefined || this.token == null || this.token === ""){
+            console.error("Missing token 🤑, please go to https://llg.com and get one");
+        }else{
             
-            elements.push(JSON.stringify(element))
-            
+        if (arguments.length == 0) {
+    
+            console.log("Grap a 🍺", " nothing to output!");
+    
         }
-        console.log(elements.join(" ➡️  "))
+        else if (arguments.length == 1) {
+            for (let index = 0; index < arguments.length; index++) {
+                const element = arguments[index];
+                console.log("🤑  " + JSON.stringify(element));
+    
+            }
+        }
+        else {
+            let elements = []
+    
+            for (let index = 0; index < arguments.length; index++) {
+                const element = arguments[index];
+    
+                elements.push(JSON.stringify(element))
+    
+            }
+            console.log(elements.join(" ➡️  "))
+        }
+        }
     }
 }
 
-module.exports.llg = llg;
+
+const _Llg = Llg;
+export { _Llg as Llg };
